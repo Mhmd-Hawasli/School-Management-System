e35142f6c8a2b5a94f3ed24debbc7c8f2cf49d16
Mhmd-Hawasli <mhmd.hawasli@gmail.com>
2024-09-24 17:32:25 +0300

    testing on tab

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -10,12 +10,12 @@
 <div class="page-title">
     <div class="row">
         <div class="col-sm-6">
-            <h4 class="mb-0"> ncvlxcnvxcnvxcv</h4>
+            <h4 class="mb-0"> page content</h4>
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb pt-0 pr-0 float-left float-sm-right ">
                 <li class="breadcrumb-item"><a href="#" class="default-color">Home</a></li>
-                <li class="breadcrumb-item active">Page Title</li>
+                <li class="breadcrumb-item active">Page Title 2</li>
             </ol>
         </div>
     </div>
